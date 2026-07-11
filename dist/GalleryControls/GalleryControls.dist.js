cffb1ace3b9a3f953f0d290a3d2136c1404e4089
Leesa Ward <leesa@doubleedesign.com.au>
2026-07-11 22:13:58 +1000

    0.1.2 Add additional gallery controls and fix CSS

--- dist/GalleryControls/GalleryControls.dist.js
+++ dist/GalleryControls/GalleryControls.dist.js
@@ -1,14 +1,33 @@
 import { FieldTooltip } from '../FieldTooltip/FieldTooltip.dist.js';
 
-const { PanelBody, ToggleControl } = wp.components;const GalleryControls = ({ name, attributes, setAttributes }) => {
+const { __experimentalToggleGroupControl, __experimentalToggleGroupControlOption, PanelBody, ToggleControl } = wp.components;const { useState } = wp.element;const GalleryControls = ({ name, attributes, setAttributes }) => {
     if (name !== 'comet/gallery') {
         return null;
     }
+    const [clickBehaviour, setClickBehaviour] = useState('none');
+    const ToggleGroupControl = __experimentalToggleGroupControl;
+    const ToggleGroupControlOption = __experimentalToggleGroupControlOption;
     return (wp.element.createElement(PanelBody, { title: "Gallery options", initialOpen: true },
-        wp.element.createElement(ToggleControl, { checked: attributes.lightbox, label: wp.element.createElement(wp.element.Fragment, null,
-                wp.element.createElement("span", null, "Enable lightbox"),
-                wp.element.createElement(FieldTooltip, { tooltip: 'When a visitor clicks on an image, open a larger version in an overlay' })), onChange: (value) => setAttributes({ lightbox: value }) }),
-        wp.element.createElement(ToggleControl, { checked: attributes.captions, label: "Show image captions if available", onChange: (value) => setAttributes({ captions: value }) })));
+        wp.element.createElement(ToggleGroupControl, { className: "comet-toggle-group", __next40pxDefaultSize: true, isBlock: true, label: "Click behaviour", onChange: (value) => {
+                setClickBehaviour(value ?? 'none');
+                switch (value) {
+                    case 'lightbox':
+                        setAttributes({ lightbox: true, externalLinks: false });
+                        break;
+                    case 'links':
+                        setAttributes({ lightbox: false, externalLinks: true });
+                        break;
+                    default:
+                        setAttributes({ lightbox: false, externalLinks: false });
+                }
+            }, value: clickBehaviour },
+            wp.element.createElement(ToggleGroupControlOption, { label: "None", value: "none" }),
+            wp.element.createElement(ToggleGroupControlOption, { label: "Lightbox", value: "lightbox", showTooltip: true, "aria-label": 'When a visitor clicks on an image, open a larger version in an overlay' }),
+            wp.element.createElement(ToggleGroupControlOption, { label: "Links", value: "links", showTooltip: true, "aria-label": 'When a visitor clicks on an image that has an external URL set, open that URL in a new tab' })),
+        wp.element.createElement(ToggleControl, { checked: attributes.captions, label: "Show image captions if available", onChange: (value) => setAttributes({ captions: value }) }),
+        wp.element.createElement(ToggleControl, { checked: attributes.imageCrop, label: wp.element.createElement(wp.element.Fragment, null,
+                wp.element.createElement("span", null, "Crop images"),
+                wp.element.createElement(FieldTooltip, { tooltip: 'If enabled, images will scale and be centered in the available space; if disabled they will be cropped to fill the space. This setting does not affect the image when open in the lightbox.' })), onChange: (value) => setAttributes({ imageCrop: value }) })));
 };
 
 export { GalleryControls };
--- src/components/GalleryControls/GalleryControls.tsx
+++ src/components/GalleryControls/GalleryControls.tsx
@@ -1,11 +1,19 @@
 import { FieldTooltip } from '../FieldTooltip/FieldTooltip';
-import { PanelBody, ToggleControl } from '@wordpress/components';
+import {
+	__experimentalToggleGroupControl,
+	__experimentalToggleGroupControlOption,
+	PanelBody,
+	ToggleControl
+} from '@wordpress/components';
+import { useState } from '@wordpress/element';
 import { EditorControlProps } from '../types';
 
 export type GalleryControlsProps = EditorControlProps & {
 	attributes: {
 		lightbox: boolean,
-		captions: boolean
+		externalLinks: boolean,
+		captions: boolean,
+		imageCrop: boolean,
 	}
 };
 
@@ -13,25 +21,67 @@ export const GalleryControls = ({ name, attributes, setAttributes }) => {
 	if (name !== 'comet/gallery') {
 		return null;
 	}
+	const [clickBehaviour, setClickBehaviour] = useState<string>('none');
+
+	const ToggleGroupControl = __experimentalToggleGroupControl;
+	const ToggleGroupControlOption = __experimentalToggleGroupControlOption;
 
 	return (
 		<PanelBody title="Gallery options" initialOpen={true}>
-			<ToggleControl
-				checked={attributes.lightbox}
-				label={
-					<>
-						<span>Enable lightbox</span>
-						<FieldTooltip
-							tooltip={'When a visitor clicks on an image, open a larger version in an overlay'}
-						/>
-					</>
-				}
-				onChange={(value) => setAttributes({ lightbox: value })}
-			/>
+			<ToggleGroupControl
+				className="comet-toggle-group"
+				__next40pxDefaultSize
+				isBlock
+				label="Click behaviour"
+				onChange={(value) => {
+					setClickBehaviour((value as string|undefined) ?? 'none');
+
+					switch(value) {
+						case 'lightbox':
+							setAttributes({ lightbox: true, externalLinks: false });
+							break;
+						case 'links':
+							setAttributes({ lightbox: false, externalLinks: true });
+							break;
+						default:
+							setAttributes({ lightbox: false, externalLinks: false });
+					}
+				}}
+				value={clickBehaviour}
+			>
+				<ToggleGroupControlOption
+					label="None"
+					value="none"
+				/>
+				<ToggleGroupControlOption
+					label="Lightbox"
+					value="lightbox"
+					showTooltip
+					aria-label={'When a visitor clicks on an image, open a larger version in an overlay'}
+				/>
+				<ToggleGroupControlOption
+					label="Links"
+					value="links"
+					showTooltip
+					aria-label={'When a visitor clicks on an image that has an external URL set, open that URL in a new tab'}
+				/>
+			</ToggleGroupControl>
 			<ToggleControl
 				checked={attributes.captions}
 				label="Show image captions if available"
 				onChange={(value) => setAttributes({ captions: value })}
+			/>
+			<ToggleControl
+				checked={attributes.imageCrop}
+				label={
+					<>
+						<span>Crop images</span>
+						<FieldTooltip
+							tooltip={'If enabled, images will scale and be centered in the available space; if disabled they will be cropped to fill the space. This setting does not affect the image when open in the lightbox.'}
+						/>
+					</>
+				}
+				onChange={(value) => setAttributes({ imageCrop: value })}
 			/>
 		</PanelBody>
 	);
